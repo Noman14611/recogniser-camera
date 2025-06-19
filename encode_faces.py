1f6ef842561156d1c54de8705c4749f96372901b
Noman14611 <noumantareen316@gmail.com>
2025-06-19 21:52:42 +0500

    encode_faces.py

--- encode_faces.py
+++ encode_faces.py
@@ -1,1 +1,25 @@
+import os
+import face_recognition
+import pickle
+
+KNOWN_FACES_DIR = "known_faces"
+ENCODINGS_FILE = "face_encodings.pkl"
+
+known_encodings = []
+known_names = []
+
+for filename in os.listdir(KNOWN_FACES_DIR):
+    if filename.endswith(".jpg") or filename.endswith(".png"):
+        image_path = os.path.join(KNOWN_FACES_DIR, filename)
+        image = face_recognition.load_image_file(image_path)
+        encoding = face_recognition.face_encodings(image)
+
+        if encoding:
+            known_encodings.append(encoding[0])
+            known_names.append(os.path.splitext(filename)[0])
+
+with open(ENCODINGS_FILE, "wb") as f:
+    pickle.dump((known_encodings, known_names), f)
+
+print("Encodings saved.")
 
